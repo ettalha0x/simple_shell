b6ae561c748d1dfe2b477bc2e6417b76fe114f8f
Noureddine Ettalhaouy <ettalhaoui151201@gmail.com>
2023-02-22 20:48:51 +0100

    I gussi finished this project

---
+++ print_env.c
@@ -0,0 +1,19 @@
+#include "simple_shell.h"
+
+/**
+* print_env - prints the environment string to stdout
+*
+* Return: 0
+*/
+void print_env(void)
+{
+	int i = 0;
+	char **env = environ;
+
+	while (env[i])
+	{
+		write(STDOUT_FILENO, (const void *)env[i], _strlen(env[i]));
+		write(STDOUT_FILENO, "\n", 1);
+		i++;
+	}
+}
--- simple_shell.c
+++ simple_shell.c
@@ -1,58 +1,50 @@
 #include "simple_shell.h"
+/**
+* main - carries out the read, execute then print output loop
+* @ac: argument count
+* @av: argument vector
+* @envp: environment vector
+*
+* Return: 0
+*/
 
-#define BUFFER_SIZE 1024
-
-int	main()
+int main(int ac, char **av, char *envp[])
 {
-	char	**envp;
-	char	**cmd;
-	char	*line = NULL;
-	char	*args[2];
-	size_t	line_size = 0;
-
+	char *line = NULL, *pathcommand = NULL, *path = NULL;
+	size_t buff_size = 0;
+	ssize_t line_size = 0;
+	char **command = NULL, **paths = NULL;
+	(void)envp, (void)av;
+	if (ac < 1)
+		return (-1);
+	signal(SIGINT, handle_signal);
 	while (1)
 	{
-		// Display prompt
-		printf("simple_shell$ ");
-		// Read user input
-		if (getline(&line, &line_size, stdin) == -1)
-		{
-			// End of file condition
-			printf("\n");
+		free_buffers(command);
+		free_buffers(paths);
+		free(pathcommand);
+		prompt_user();
+		line_size = getline(&line, &buff_size, stdin);
+		if (line_size < 0)
 			break;
-		}
-		// Remove trailing newline character
-		line[strcspn(line, "\n")] = '\0';
-		// Fork process
-		pid_t pid = fork();
-		if (pid == -1)
-		{
-			// Fork failed
-			perror("fork");
-			exit(EXIT_FAILURE);
-		}
-		else if (pid == 0)
-		{
-			// Child process
-			cmd = ft_split(line, ' ');
-			args[0] = cmd[0];
-			args[1] = NULL; //cmd[1]; trying to handle command lines with arguments
-			//this is just for test
-			// printf("line =%s\nline size = %lu\n", line, line_size);
-			// printf("cmd[0] =%s\ncmd[1] = %s\n\n", cmd[0], cmd[1]);
-			execve(args[0], args, envp);
-			// execve only returns if an error occurs
-			perror("Error");
-			exit(EXIT_FAILURE);
-		}
+		info.ln_count++;
+		if (line[line_size - 1] == '\n')
+			line[line_size - 1] = '\0';
+		command = tokenizer(line);
+		if (command == NULL || *command == NULL || **command == '\0')
+			continue;
+		if (checker(command, line))
+			continue;
+		path = find_path();
+		paths = tokenizer(path);
+		pathcommand = test_path(paths, command[0]);
+		if (!pathcommand)
+			perror(av[0]);
 		else
-		{
-			// Parent process
-			wait(NULL);
-		}
+			execution(pathcommand, command);
 	}
-
+	if (line_size < 0 && flags.interactive)
+		write(STDERR_FILENO, "\n", 1);
 	free(line);
-	exit(EXIT_SUCCESS);
+	return (0);
 }
-
